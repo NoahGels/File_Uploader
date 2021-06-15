a8437c32441b43ab8d4fe42e9b1ba9bc4b37965e
NoahGels <noah.gels@gg-lin.de>
2021-06-15 16:41:14 +0200

    Added Prize Calculator

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const PORT = 50007
 const keygen = require('keygenerator')
 const fs = require('fs')
 
+const items = JSON.parse(fs.readFileSync(__dirname + '/files/products.json'))
+
 app.set('view engine', 'ejs')
 app.set('views', 'files/pages')
 
@@ -24,6 +26,32 @@ app.get('/download/:id', async (req, res) => {
 
     // download the file which is probably existing now
     res.status(200).download(__dirname + '/files/uploads/' + uploads[req.params.id])
+})
+
+app.get('/preise', (req, res) => {
+
+    let total = 0
+    items.forEach((item) => total += item.prize)
+    const cost = total / 43
+
+    res.render('prize-list', {
+        product: req.query.product,
+        prize: req.query.prize,
+        items: items,
+        cost: cost.toFixed(2),
+        total: total.toFixed(2),
+        error: req.query.error
+    })
+})
+
+app.get('/addItem', (req, res) => {
+    if(!(req.query.prize * 1)) {
+        res.redirect('/preise?error=' + 1)
+        return
+    }
+    items.splice(0, 0, { product: req.query.product,  prize: req.query.prize * 1})
+    fs.writeFileSync(__dirname + '/files/products.json', JSON.stringify(items))
+    res.redirect('/preise?product=' + req.query.product + '&prize=' + req.query.prize)
 })
 
 app.get('/:id', async (req, res) => {
